ac94e4a83059972cd2c1b2c1bf7718c89799a72d
Siddharth Arora <sidd.arora99@gmail.com>
2019-08-27 18:40:42 +0530

    Without Projets

---
+++ app.js
@@ -0,0 +1,14 @@
+const express= require("express");
+const bodyparser= require("body-parser");
+const app=express();
+app.use(express.static('public'));
+var port=process.env.PORT||3000
+
+app.get("/",function(req,res)
+{
+	res.sendFile(__dirname+"/index.html");
+});
+app.listen(port,function()
+{
+	console.log("server started at port 3000");
+});
