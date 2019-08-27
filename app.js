be2672136d3a08089b10abff2adbf4389cd86adb
Siddharth Arora <sidd.arora99@gmail.com>
2019-08-27 20:33:01 +0530

    ub

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express= require("express");
 const bodyparser= require("body-parser");
 const app=express();
 app.use(express.static('public'));
-var port=process.env.PORT||3000
-
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+var port=process.env.PORT||3000;
 app.get("/",function(req,res)
 {
 	res.sendFile(__dirname+"/index.html");
