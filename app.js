67a856c0a7fd8d0206c1520a80e8074b1ba4c81b
Siddharth Arora <sidd.arora99@gmail.com>
2019-08-27 20:23:07 +0530

    Content Updated

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const app=express();
 app.use(express.static('public'));
 var port=process.env.PORT||3000
 
+
 app.get("/",function(req,res)
 {
 	res.sendFile(__dirname+"/index.html");
 });
+
+
 app.listen(port,function()
 {
 	console.log("server started at port 3000");
